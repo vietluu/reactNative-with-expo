724e4061bedfa39e2e78fcf356433b44f845b315
quangtv <quangtv@rabiloo.com>
2023-04-24 16:19:01 +0700

    update

--- components/post/PostItem.tsx
+++ components/post/PostItem.tsx
@@ -1,11 +1,17 @@
-import { Text, View } from "native-base"
+import { Icon, Text, View } from "native-base"
 
 const PostItem = () => {
     return (
         <View className="flex flex-col bg-slate-400 w-full">
             <View>Header</View>
             <Text>Hi</Text>
-            <View>actions</View>
+            <View>
+                <View className="h-full flex flex-row gap-2">
+                    <Icon name="home" size={6} className="mx-0"/>
+                    <Icon name="plus" size={6}/>
+                    <Icon name="plus" size={6}/>
+                </View>
+            </View>
         </View>
     )
 }
