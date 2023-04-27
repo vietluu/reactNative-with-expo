e6f8b11aa3341ae27782ab8ec13b8e62bdec1ae8
sann <psang4845@gmail.com>
2023-04-27 13:49:33 +0700

    giao dien Login Register

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -1,20 +1,48 @@
 import React from 'react'
-import { FormControl, Input, Stack, Text, Box, Center, NativeBaseProvider, WarningOutlineIcon } from "native-base"
+import { FormControl, Input, Stack, Text, Box, Button, Center, NativeBaseProvider, WarningOutlineIcon } from "native-base"
 
 
 type Props = {}
 
 const Login = (props: Props) => {
   return (
-   <Box>
-      <FormControl isInvalid>
-        <FormControl.Label>Email</FormControl.Label>
-        <Input placeholder="Enter Email" />
-        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-          Something is wrong.
-        </FormControl.ErrorMessage>
-      </FormControl>
-      </Box>
+    <>
+      <Text className='mb-5 text-lg font-bold'>Login</Text>
+      <Stack space={4} w="75%" maxW="300px" mx="auto">
+        <Input shadow={2} _light={{
+        bg: "coolGray.100",
+        _hover: {
+          bg: "coolGray.200"
+        },
+        _focus: {
+          bg: "coolGray.200:alpha.70"
+        }
+          }}
+            placeholder="Enter your name" />
+      
+
+        <Input shadow={2} _light={{
+          bg: "coolGray.100",
+          _hover: {
+            bg: "coolGray.200"
+          },
+          _focus: {
+            bg: "coolGray.200:alpha.70"
+          }
+            }}
+                placeholder="Enter your Password" />
+      </Stack>
+
+      <Stack mb="2.5" mt="2" direction={{
+        base: "column",
+        md: "row"
+      }} space={1} mx={{
+        base: "auto",
+        md: "3"
+      }}>
+        <Button size="md">Login</Button>   
+      </Stack>
+    </>
   )
 }
 
--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -1,10 +1,59 @@
 import React from 'react'
+import { FormControl, Input, Stack, Text, Box, Button, Center, NativeBaseProvider, WarningOutlineIcon } from "native-base"
+
 
 type Props = {}
 
 const Register = (props: Props) => {
   return (
-    <div>Register</div>
+    <>
+      <Text className='mb-5 text-lg font-bold'>Register</Text>
+      <Stack space={4} w="75%" maxW="300px" mx="auto">
+    <Input shadow={2} _light={{
+    bg: "coolGray.100",
+    _hover: {
+      bg: "coolGray.200"
+    },
+    _focus: {
+      bg: "coolGray.200:alpha.70"
+    }
+      }}
+        placeholder="Enter your name" />
+      
+
+  <Input shadow={2} _light={{
+    bg: "coolGray.100",
+    _hover: {
+      bg: "coolGray.200"
+    },
+    _focus: {
+      bg: "coolGray.200:alpha.70"
+    }
+      }}
+          placeholder="Enter your Password" />
+        
+  <Input shadow={2} _light={{
+    bg: "coolGray.100",
+    _hover: {
+      bg: "coolGray.200"
+    },
+    _focus: {
+      bg: "coolGray.200:alpha.70"
+    }
+      }}
+          placeholder="Enter your Conirm Password" />
+      </Stack>
+
+       <Stack mb="2.5" mt="2" direction={{
+        base: "column",
+        md: "row"
+      }} space={1} mx={{
+        base: "auto",
+        md: "3"
+      }}>
+          <Button size="md">Register</Button>   
+    </Stack>
+    </>
   )
 }
 
