d9f017a657752d82d6e4b7e6acbd427bc1505cd3
quangtv <quangtv@rabiloo.com>
2023-04-28 14:21:37 +0700

    update structure

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import {
 } from 'native-base';
 import NativeBaseIcon from './src/lib/NativeBaseIcon';
 import PostItem from './src/screens/home/PostItem';
-import AvatarEntity from './src/common/AvatarEntity';
+import AvatarEntity from './src/components/common/AvatarEntity';
 import PostLoader from './src/components/PostLoader';
 import CommentLoader from './src/components/CommentLoader';
 import Navigation from './src/components/Navigation';
