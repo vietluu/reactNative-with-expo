24a16d2060291fd7a77364e16f4ac7147e03b9f2
sann <psang4845@gmail.com>
2023-05-02 16:37:57 +0700

    validate SignIn SignUp

--- src/screens/auth/Signin.tsx
+++ src/screens/auth/Signin.tsx
@@ -5,39 +5,57 @@ import { setToken } from '../../utils/token'
 import { UserSignIn } from '../../types'
 import { MaterialIcons } from '@expo/vector-icons'
 import { Input, Text, Button, Icon, Pressable, Center, FormControl } from 'native-base'
-import { Formik } from 'formik'
-import * as Yup from 'yup'
 
-interface Values {
-  email: string
-  password: string
-}
+
+
 
 const SignIn = ({ navigation }: any) => {
   const [show, setShow] = useState(false)
   const [loading, setLoading] = useState(false)
   const [user, setUser] = useState({ email: '', password: '' })
-
-
-  //SignInSchema
-  const SignInSchema = Yup.object().shape({
-    email: Yup.string()
-      .email('Invalid email address')
-      .required('Please enter your email!'),
-    password: Yup.string()
-      .min(8, 'Password must be 8 characters long!')
-      .max(16, 'Too Long!')
-      .matches(
-        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
-        "Must contain minium 8 characters including 1 uppercase letter, 1 special character, and alphanumeric characters"
-      )
-      .required('Please enter your password!'),
+  const [errors, setErrors] = useState({
+    email: '', password: '', confirmPassword: ''
   })
+
+
+
+  const validate = (user: any) => {
+    const error = {
+      email: "",
+      password: "",
+      confirmPassword: ""
+
+    }
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    const passwordRegex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})/
+
+    if (user.email === "") {
+      error.email = "Please enter your Email"
+    }
+    if (!emailRegex.test(user.email)) {
+      error.email = "Email didn't match"
+    }
+
+    if (user.password === "") {
+      error.password = "Please enter your Password"
+    }
+    if (!passwordRegex.test(user.password)) {
+      error.password = "Password didn't match"
+    }
+    if (user.confirmPassword === "" || user.confirmPassword != user.password) {
+      error.confirmPassword = "Password not matched"
+
+    }
+
+    return error
+  }
+
 
   //handleSignIn
   const handleSignIn = async () => {
-
     setLoading(true)
+    setErrors(validate(user))
+
     const payload: UserSignIn = user
 
     try {
@@ -64,68 +82,56 @@ const SignIn = ({ navigation }: any) => {
         Sign In
       </Text>
 
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-          confirmPassword: '',
-        }}
-        onSubmit={(
-          values: Values
-        ) => {
-          console.log('submit');
 
-          handleSignIn()
-        }}
-        validationSchema={SignInSchema}
-      >
-
-        {({ handleSubmit, handleBlur, handleChange, errors, touched }) => (
-          <FormControl>
-            {/* Email Input */}
-            <Input
-              marginTop={4}
-              InputLeftElement={<Icon as={<MaterialIcons name="email" />} size={5} ml="2" color="muted.400" />}
-              onBlur={handleBlur("email")}
-              onChangeText={handleChange('email')}
-              defaultValue={user.email}
-              placeholder="Email"
-            // onChangeText={(text) => setUser({ ...user, email: text })}
-            />
-            {errors.email && touched.email ? <Text color="error.500">{errors.email}</Text> : null}
+      <FormControl>
+        {/* Email Input */}
+        <Input
+          marginTop={4}
+          InputLeftElement={<Icon as={<MaterialIcons name="email" />} size={5} ml="2" color="muted.400" />}
+          defaultValue={user.email}
+          placeholder="Email"
+          onChangeText={(text) => {
+            setUser({ ...user, email: text })
+          }}
+        />
+        {errors.email && <Text color="error.500">{errors.email}</Text>}
 
 
-            {/* Password Input */}
-            <Input
-              marginTop={4}
-              type={show ? 'text' : 'password'}
-              InputRightElement={
-                <Pressable onPress={() => setShow(!show)}>
-                  <Icon
-                    as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
-                    size={5}
-                    mr="2"
-                    color="muted.400"
-                  />
-                </Pressable>
-              }
-              onBlur={handleBlur("password")}
-              onChangeText={handleChange('password')}
-              defaultValue={user.password}
-              placeholder="Password"
-            // onChangeText={(text) => setUser({ ...user, password: text })}
-            />
-            {errors.email && touched.password ? <Text color="error.500">{errors.password}</Text> : null}
+        {/* Password Input */}
+        <Input
+          marginTop={4}
+          type={show ? 'text' : 'password'}
+          InputRightElement={
+            <Pressable onPress={() => setShow(!show)}>
+              <Icon
+                as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
+                size={5}
+                mr="2"
+                color="muted.400"
+              />
+            </Pressable>
+          }
+
+          defaultValue={user.password}
+          placeholder="Password"
+          onChangeText={(text) => {
+            setUser({ ...user, password: text })
+          }}
+        />
+        {errors.password && <Text color="error.500">{errors.password}</Text>}
 
 
-            <Button title="Submit" w={'full'} onPress={() => handleSubmit()} marginTop={4} isLoading={loading} isLoadingText="Signing in">
-              Sign in
-            </Button>
-
-
-          </FormControl>
-        )}
-      </Formik>
+        <Button
+          title="Submit"
+          w={'full'}
+          onPress={handleSignIn}
+          marginTop={4}
+          isLoading={loading}
+          isLoadingText="Signing in"
+        >
+          Sign in
+        </Button>
+      </FormControl>
 
 
       <Text onPress={goToSignUp} marginTop={2}>
--- src/screens/auth/Signup.tsx
+++ src/screens/auth/Signup.tsx
@@ -3,53 +3,29 @@ import { Input, Text, Button, Icon, Pressable, Center, FormControl, useToast } f
 import { MaterialIcons } from '@expo/vector-icons'
 import { api } from '../../utils/api'
 import { API_URL } from '@env'
-import { Formik } from 'formik'
-import * as Yup from 'yup'
+
 
 import { UserSignUp } from '../../types'
 import { AnyIfEmpty } from 'react-redux'
 
-interface Values {
-  email: string
-  password: string
-  confirmPassword: string
-}
 
 const SignUp = ({ navigation }: any) => {
   const [show, setShow] = useState(false)
   const [loading, setLoading] = useState(false)
   const [user, setUser] = useState({ email: '', password: '', confirmPassword: '' })
+  const [errors, setErrors] = useState({
+    email: '', password: '', confirmPassword: ''
+  })
+
   const toast = useToast()
 
-  //SignUpSchema
-  const SignUpSchema = Yup.object().shape({
-    email: Yup.string()
-      .email('Invalid email address')
-      .required('Please enter your email!'),
-    password: Yup.string()
-      .min(8, 'Password must be 8 characters long!')
-      .max(16, 'Too Long!')
-      .matches(
-        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
-        "Must contain minium 8 characters including 1 uppercase letter, 1 special character, and alphanumeric characters"
-      )
-      .required('Please enter your password!'),
-    confirmPassword: Yup.string()
-      .min(8, 'Confirm Password must be 8 characters long!!')
-      .max(16, 'Too Long!')
-      .matches(
-        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
-        "Must contain minium 8 characters including 1 uppercase letter, 1 special character, and alphanumeric characters"
-      )
-      .oneOf([Yup.ref("password"), "Password do not match"])
-      .required('Please enter your confirm password!')
-    ,
-  })
+
 
   //handleSignUp
   const handleSignUp = async () => {
-
     setLoading(true)
+
+    setErrors(validate(user))
 
     const payload: UserSignUp = user
 
@@ -76,98 +52,118 @@ const SignUp = ({ navigation }: any) => {
     navigation.navigate('SignInScreen')
   }
 
+
+  const validate = (user: any) => {
+    const error = {
+      email: "",
+      password: "",
+      confirmPassword: ""
+
+    }
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    const passwordRegex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})/
+
+    if (user.email === "") {
+      error.email = "Please enter your Email"
+    }
+    if (!emailRegex.test(user.email)) {
+      error.email = "Email didn't match"
+    }
+
+    if (user.password === "") {
+      error.password = "Please enter your Password"
+    }
+    if (!passwordRegex.test(user.password)) {
+      error.password = "Password didn't match"
+    }
+    if (user.confirmPassword === "" || user.confirmPassword != user.password) {
+      error.confirmPassword = "Password not matched"
+
+    }
+
+    return error
+  }
+
+
+
   return (
     <Center _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} px={4} flex={1}>
       <Text fontSize={'xl'} fontWeight={'medium'}>
         Sign Up
       </Text>
 
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-          confirmPassword: '',
-        }}
-        onSubmit={(
-          values: Values
-        ) => {
-          console.log('submit');
 
-          handleSignUp()
-        }}
-        validationSchema={SignUpSchema}
-      >
+      <FormControl>
+        {/* Email Input */}
+        <Input
+          marginTop={4}
+          InputLeftElement={<Icon as={<MaterialIcons name="email" />} size={5} ml="2" color="muted.400" />}
+          placeholder="Email"
+          defaultValue={user.email}
+          onChangeText={(text) => {
+            setUser({ ...user, email: text })
+          }}
+        />
+        {errors.email && <Text color="error.500">{errors.email}</Text>}
 
-        {({ handleSubmit, handleBlur, handleChange, errors, touched }) => (
-          <FormControl>
-            {/* Email Input */}
-            <Input
-              marginTop={4}
-              InputLeftElement={<Icon as={<MaterialIcons name="email" />} size={5} ml="2" color="muted.400" />}
-              placeholder="Email"
-              onBlur={handleBlur("email")}
-              onChangeText={handleChange('email')}
-              defaultValue={user.email}
-            // onChangeText={(text) => setUser({ ...user, email: text })}
-            // value={values.email}
-            />
-            {errors.email && touched.email ? <Text color="error.500">{errors.email}</Text> : null}
-
-            {/* PassWord Input */}
-            <Input
-              marginTop={4}
-              type={show ? 'text' : 'password'}
-              InputRightElement={
-                <Pressable onPress={() => setShow(!show)}>
-                  <Icon
-                    as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
-                    size={5}
-                    mr="2"
-                    color="muted.400"
-                  />
-                </Pressable>
-              }
-              placeholder="Password"
-              onBlur={handleBlur("Password")}
-              onChangeText={handleChange('password')}
-              defaultValue={user.password}
-            // onChangeText={(text) => setUser({ ...user, password: text })}
-            // value={values.password}
-            />
-            {errors.password && touched.password ? <Text color="error.500">{errors.password}</Text> : null}
-
-            {/* Confirm PassWord Input */}
-            <Input
-              marginTop={4}
-              type={show ? 'text' : 'password'}
-              InputRightElement={
-                <Pressable onPress={() => setShow(!show)}>
-                  <Icon
-                    as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
-                    size={5}
-                    mr="2"
-                    color="muted.400"
-                  />
-                </Pressable>
-              }
-              placeholder="Confirm Password"
-              onBlur={handleBlur("confirmPassword")}
-              onChangeText={handleChange('confirmPassword')}
-              defaultValue={user.confirmPassword}
-            // onChangeText={(text) => setUser({ ...user, confirmPassword: text })}
-            // value={values.confirmPassword}
-            />
-            {errors.confirmPassword && touched.confirmPassword ? <Text color="error.500">{errors.confirmPassword}</Text> : null}
-
-            <Button w={'full'} title="Submit" onPress={() => handleSubmit()} marginTop={4} isLoading={loading} isLoadingText="Signing up">
-              Sign Up
-            </Button>
+        {/* PassWord Input */}
+        <Input
+          marginTop={4}
+          type={show ? 'text' : 'password'}
+          InputRightElement={
+            <Pressable onPress={() => setShow(!show)}>
+              <Icon
+                as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
+                size={5}
+                mr="2"
+                color="muted.400"
+              />
+            </Pressable>
+          }
+          placeholder="Password"
+          defaultValue={user.password}
+          onChangeText={(text) => {
+            setUser({ ...user, password: text })
+          }}
+        />
+        {errors.password && <Text color="error.500">{errors.password}</Text>}
 
 
-          </FormControl>
-        )}
-      </Formik>
 
+        {/* Confirm PassWord Input */}
+        <Input
+          marginTop={4}
+          type={show ? 'text' : 'password'}
+          InputRightElement={
+            <Pressable onPress={() => setShow(!show)}>
+              <Icon
+                as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
+                size={5}
+                mr="2"
+                color="muted.400"
+              />
+            </Pressable>
+          }
+          placeholder="Confirm Password"
+          defaultValue={user.confirmPassword}
+          onChangeText={(text) => {
+            setUser({ ...user, confirmPassword: text })
+          }}
+
+        />
+        {errors.confirmPassword && <Text color="error.500">{errors.confirmPassword}</Text>}
+
+        <Button
+          w={'full'}
+          title="Submit"
+          onPress={handleSignUp}
+          marginTop={4}
+          isLoading={loading}
+          isLoadingText="Signing up"
+        >
+          Sign Up
+        </Button>
+      </FormControl>
 
 
       <Text onPress={goToSignIn} marginTop={2}>
